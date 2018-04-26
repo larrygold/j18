0a5507ca8bddff7050faf305d5df5c6deec207b1
larrygold <larry@goldstein.fr>
2018-04-26 19:17:56 +0200

    19h17

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,14 @@ class UsersController < ApplicationController
     puts @name
     puts @bio
     User.create(username: @name, bio: @bio)
+    @id = User.last.id
+    redirect_to show_user_path(id:@id)
+  end
+
+  def show
+    @id = params[:id]
+    @name = User.find(@id).username
+    @bio = User.find(@id).bio
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/about(/:who)', to: 'static_pages#about', as: 'about'
   get 'users/new', to: 'users#new', as: 'register'
   post 'users/new', to: 'users#create', as: 'create_account'
+  get 'users/:id', to: 'users#show', as: 'show_user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
