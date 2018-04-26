da4c6ab1218b71e349f4a2a727d1b0e5f5950ab7
larrygold <larry@goldstein.fr>
2018-04-26 17:44:14 +0200

    17h44

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def create
+    @name = params[:name]
+    @bio = params[:bio]
+    puts @name
+    puts @bio
+    User.create(username: @name, bio: @bio)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root to: 'static_pages#home'
   get '/contact', to: 'static_pages#contact', as: 'contact'
   get '/about(/:who)', to: 'static_pages#about', as: 'about'
+  get 'users/new', to: 'users#new', as: 'register'
+  post 'users/new', to: 'users#create', as: 'create_account'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
